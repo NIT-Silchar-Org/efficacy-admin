28079d369ba9b162b9f4717265c65f2dec2022c3
Apoorv Aadarsh <71458873+apoorvaadarsh@users.noreply.github.com>
2020-11-04 02:30:41 +0530

    fixed dropdown menu

--- lib/widgets/signup_files/dropdown_menu.dart
+++ lib/widgets/signup_files/dropdown_menu.dart
@@ -1,7 +1,7 @@
-import 'package:cmApp/providers/dropDownItem_provider.dart';
+//import 'package:cmApp/providers/dropDownItem_provider.dart';
 import 'package:flutter/material.dart';
 
-import 'package:provider/provider.dart';
+//import 'package:provider/provider.dart';
 
 class DropdownMenu extends StatefulWidget {
   final Size deviceSize;
@@ -23,50 +23,43 @@ class _DropdownMenuState extends State<DropdownMenu> {
   @override
   void initState() {
     _itemList = widget.itemList;
-    _dropDownValue = _itemList[0];
+    //_dropDownValue = _itemList[0];
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            color: (_dropDownValue == _itemList[0])
-                ? Colors.grey
-                : Colors.deepPurpleAccent,
+    return FormField<String>(
+      builder: (FormFieldState<String> state) {
+        return InputDecorator(
+          decoration: InputDecoration(
+            labelStyle: Theme.of(context).textTheme.subtitle1,
+            errorStyle: TextStyle(color: Colors.redAccent, fontSize: 16.0),
+            hintText: 'Please select ${widget.itemType}',
+            //  border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
           ),
-        ),
-      ),
-      width: widget.deviceSize.width * 0.85,
-      child: ButtonTheme(
-        alignedDropdown: true,
-        child: DropdownButtonFormField<String>(
-          isExpanded: true,
-          value: _dropDownValue,
-          icon: Icon(
-            Icons.arrow_drop_down,
-            color: Theme.of(context).textTheme.button.color,
+          isEmpty: _dropDownValue == null,
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton<String>(
+              value: _dropDownValue,
+              isDense: true,
+              onChanged: (String selectedValue) {
+                setState(() {
+                  _dropDownValue = selectedValue;
+                  widget.adminCredentials[widget.itemType] = selectedValue;
+                  state.didChange(selectedValue);
+                });
+              },
+              items: _itemList.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
           ),
-          iconSize: 35,
-          elevation: 6,
-          style: Theme.of(context).textTheme.subtitle1,
-          decoration: InputDecoration(border: InputBorder.none),
-          items: _itemList.map((club) {
-            return DropdownMenuItem<String>(
-              value: club,
-              child: Text(club),
-            );
-          }).toList(),
-          onChanged: (String selectedValue) {
-            setState(() {
-              _dropDownValue = selectedValue;
-              widget.adminCredentials[widget.itemType] = _dropDownValue;
-            });
-          },
-        ),
-      ),
+        );
+      },
     );
   }
 }
