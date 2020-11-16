64ff8e45f26407e778aa3860107a044a042fc495
Arjun Sah <arjun801110@gmail.com>
2020-11-16 16:19:49 +0530

    created add_event.dart and assets folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,25 @@
 import 'package:dsc_club_management_app/signup.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'login.dart';
-import 'homepage.dart';
+import 'package:flutter/material.dart';
 
-void main() async{
+import 'homepage.dart';
+import 'login.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home:login(),
-      routes: {
-        '/signup.dart':(_)=>signup(),
-        '/homepage':(_)=>homePage()
-      },
+      debugShowCheckedModeBanner: false,
+      home: login(),
+      routes: {'/signup.dart': (_) => signup(), '/homepage': (_) => homePage()},
     );
-
   }
-
 }
