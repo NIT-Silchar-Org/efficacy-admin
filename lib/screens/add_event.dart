ce782a7514360d1af02116473f3057cd65595bb3
Arjun Sah <arjun801110@gmail.com>
2020-11-16 18:42:15 +0530

    added some input fields

--- lib/screens/add_event.dart
+++ lib/screens/add_event.dart
@@ -26,17 +26,6 @@ class _AddEventState extends State<AddEvent> {
                     alignment: Alignment.center,
                     child: Image.asset("assets/bg_top_layer.png"),
                   ),
-                  // Container(
-                  //   alignment: Alignment.center,
-                  //   child: Text(
-                  //     "Add Event",
-                  //     style: GoogleFonts.openSans(
-                  //         color: Colors.white,
-                  //         letterSpacing: .5,
-                  //         fontSize: 41,
-                  //         fontWeight: FontWeight.bold),
-                  //   ),
-                  // )
                   Positioned(
                     right: 21.0,
                     bottom: 31.0,
@@ -57,8 +46,323 @@ class _AddEventState extends State<AddEvent> {
                         ],
                       ),
                     ),
-                  ),
+                  )
                 ],
+              ),
+              //Title
+              Container(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 21, top: 21, right: 21, bottom: 11),
+                    child: Text(
+                      "Title",
+                      style: GoogleFonts.roboto(
+                        textStyle: TextStyle(
+                            color: Colors.blue[900],
+                            letterSpacing: 0.5,
+                            fontSize: 21,
+                            fontWeight: FontWeight.w900),
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 21, top: 0, right: 21, bottom: 21),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.cyan[50],
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 5.0,
+                        color: Colors.blue[400],
+                      ),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(11),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero,
+                          topLeft: Radius.circular(11)),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 5.0,
+                        color: Colors.blue[900],
+                      ),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(11),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero,
+                          topLeft: Radius.circular(11)),
+                    ),
+                  ),
+                  keyboardType: TextInputType.text,
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      color: Colors.blue[900],
+                      letterSpacing: 0.5,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              //Description
+              Container(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 21, top: 11, right: 21, bottom: 11),
+                    child: Text(
+                      "Description",
+                      style: GoogleFonts.roboto(
+                        textStyle: TextStyle(
+                            color: Colors.blue[900],
+                            letterSpacing: 0.5,
+                            fontSize: 21,
+                            fontWeight: FontWeight.w900),
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 21, top: 0, right: 21, bottom: 21),
+                child: TextFormField(
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.cyan[50],
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 5.0,
+                        color: Colors.blue[400],
+                      ),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(11),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero,
+                          topLeft: Radius.circular(11)),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 5.0,
+                        color: Colors.blue[900],
+                      ),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(11),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero,
+                          topLeft: Radius.circular(11)),
+                    ),
+                  ),
+                  keyboardType: TextInputType.text,
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      color: Colors.blue[900],
+                      letterSpacing: 0.5,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              //Venue
+              Container(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 21, top: 11, right: 21, bottom: 11),
+                    child: Text(
+                      "Venue",
+                      style: GoogleFonts.roboto(
+                        textStyle: TextStyle(
+                            color: Colors.blue[900],
+                            letterSpacing: 0.5,
+                            fontSize: 21,
+                            fontWeight: FontWeight.w900),
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 21, top: 0, right: 21, bottom: 21),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.cyan[50],
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 5.0,
+                        color: Colors.blue[400],
+                      ),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(11),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero,
+                          topLeft: Radius.circular(11)),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 5.0,
+                        color: Colors.blue[900],
+                      ),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(11),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero,
+                          topLeft: Radius.circular(11)),
+                    ),
+                  ),
+                  keyboardType: TextInputType.text,
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      color: Colors.blue[900],
+                      letterSpacing: 0.5,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              //Date
+              Container(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 21, top: 11, right: 21, bottom: 11),
+                    child: Text(
+                      "Date",
+                      style: GoogleFonts.roboto(
+                        textStyle: TextStyle(
+                            color: Colors.blue[900],
+                            letterSpacing: 0.5,
+                            fontSize: 21,
+                            fontWeight: FontWeight.w900),
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 21, top: 0, right: 21, bottom: 21),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.cyan[50],
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 5.0,
+                        color: Colors.blue[400],
+                      ),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(11),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero,
+                          topLeft: Radius.circular(11)),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 5.0,
+                        color: Colors.blue[900],
+                      ),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(11),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero,
+                          topLeft: Radius.circular(11)),
+                    ),
+                  ),
+                  keyboardType: TextInputType.text,
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      color: Colors.blue[900],
+                      letterSpacing: 0.5,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              //Time
+              Container(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 21, top: 11, right: 21, bottom: 11),
+                    child: Text(
+                      "Time",
+                      style: GoogleFonts.roboto(
+                        textStyle: TextStyle(
+                            color: Colors.blue[900],
+                            letterSpacing: 0.5,
+                            fontSize: 21,
+                            fontWeight: FontWeight.w900),
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 21, top: 0, right: 21, bottom: 21),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.cyan[50],
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 5.0,
+                        color: Colors.blue[400],
+                      ),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(11),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero,
+                          topLeft: Radius.circular(11)),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 5.0,
+                        color: Colors.blue[900],
+                      ),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(11),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero,
+                          topLeft: Radius.circular(11)),
+                    ),
+                  ),
+                  keyboardType: TextInputType.text,
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      color: Colors.blue[900],
+                      letterSpacing: 0.5,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              //Add Button
+              RaisedButton(
+                onPressed: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(11),
+                  child: Text(
+                    "Add",
+                    style: GoogleFonts.openSans(
+                      textStyle: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: .5,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                color: Colors.blue[900],
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(51)),
+              ),
+              SizedBox(
+                height: 21,
               ),
             ],
           ),
