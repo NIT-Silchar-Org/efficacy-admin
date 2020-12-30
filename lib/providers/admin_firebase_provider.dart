4550b5315684e3f0a337ad6037b4c54b66a4410e
Apoorv <apoorvsahay10@gmail.com>
2020-12-30 22:06:54 +0530

    minor fixes

--- lib/providers/admin_firebase_provider.dart
+++ lib/providers/admin_firebase_provider.dart
@@ -4,17 +4,14 @@ import 'package:flutter/cupertino.dart';
 class AdminFirebaseProvider with ChangeNotifier {
   Future<void> userSetup(Map<String, dynamic> adminCredentials) async {
     CollectionReference adminData =
-        FirebaseFirestore.instance.collection('clubs');
+        FirebaseFirestore.instance.collection('admins');
 
     print(adminData);
 
     final Map<String, String> _adminMap = {
       'adminName': adminCredentials['name'].toString(),
       'clubName': adminCredentials['club'].toString(),
-      //'uid': adminCredentials['uid'].toString(),
-      'description': 'A very nice club',
-      'fb': null,
-      'imageUrl': null,
+      'uid': adminCredentials['uid'].toString(),
       'clubId': adminCredentials['clubId'].toString(),
     };
     adminData.add(_adminMap);
