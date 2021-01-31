d8f4baff2ba139d1997bdb3356135decb43dd192
Shreya2112-Coder <70101205+Shreya2112-Coder@users.noreply.github.com>
2021-01-31 16:43:50 +0530

    adding event details in firebase

--- lib/screens/addEvent_screen.dart
+++ lib/screens/addEvent_screen.dart
@@ -1,11 +1,20 @@
+import 'dart:collection';
 import 'dart:io';
 
+
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import 'package:image_picker/image_picker.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:intl/intl.dart';
+import 'package:date_format/date_format.dart';
 
-class AddEventScreen extends StatefulWidget { 
+
+import 'package:firebase_storage/firebase_storage.dart';
+
+class AddEventScreen extends StatefulWidget {
    static const routeName = '/add_event_screen';
   @override
   _AddEventScreenState createState() => _AddEventScreenState();
@@ -15,13 +24,101 @@ class _AddEventScreenState extends State<AddEventScreen> {
 
   final _formKey = GlobalKey<FormState>();
   File _image;
-  Future<void> pickImage() async {
-    // var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-    setState(() {
-      // _image = image;
-    });
-    // print("size = " + _image.lengthSync().toString());
+
+  String _setTime, _setDate;
+
+  String _hour, _minute, _time;
+
+  String dateTime;
+
+  DateTime selectedDate = DateTime.now();
+
+  TimeOfDay selectedTime = TimeOfDay(hour: 00, minute: 00);
+
+  TextEditingController _dateController = TextEditingController();
+  TextEditingController _timeController = TextEditingController();
+
+  TextEditingController _des, _title, _venue, _date;
+
+
+  DatabaseReference _ref;
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        initialDatePickerMode: DatePickerMode.day,
+        firstDate: DateTime(2015),
+        lastDate: DateTime(2101));
+    if (picked != null)
+      setState(() {
+        selectedDate = picked;
+        _dateController.text = DateFormat.yMd().format(selectedDate);
+      });
   }
+
+  Future<Null> _selectTime(BuildContext context) async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (picked != null)
+      setState(() {
+        selectedTime = picked;
+        _hour = selectedTime.hour.toString();
+        _minute = selectedTime.minute.toString();
+        _time = _hour + ' : ' + _minute;
+        _timeController.text = _time;
+        _timeController.text = formatDate(
+            DateTime(2019, 08, 1, selectedTime.hour, selectedTime.minute),
+            [hh, ':', nn, " ", am]).toString();
+      });
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    _des = TextEditingController();
+    _title = TextEditingController();
+    _venue = TextEditingController();
+
+
+    _dateController.text = DateFormat.yMd().format(DateTime.now());
+
+    _timeController.text = formatDate(
+        DateTime(2019, 08, 1, DateTime
+            .now()
+            .hour, DateTime
+            .now()
+            .minute),
+        [hh, ':', nn, " ", am]).toString();
+
+    _ref = FirebaseDatabase.instance.reference().child('events');
+  }
+
+  Future<void> getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = image;
+    });
+    print("size = " + _image.lengthSync().toString());
+  }
+
+  Future uploadPic(BuildContext context) async {
+    String fileName = basename(_image.path);
+    Reference firebaseStorageRef = FirebaseStorage.instance.ref().child(
+        fileName);
+    UploadTask uploadTask = firebaseStorageRef.putFile(_image);
+    TaskSnapshot taskSnapshot = await uploadTask;
+    setState(() {
+      print("poster uploaded");
+      Scaffold.of(context).showSnackBar(
+          SnackBar(content: Text('poster uploaded')));
+    });
+  }
+
+  String description, date, time, venue, image, title;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +126,6 @@ class _AddEventScreenState extends State<AddEventScreen> {
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Form(
-          key: _formKey,
           child: Column(
             children: <Widget>[
               Stack(
@@ -86,12 +182,9 @@ class _AddEventScreenState extends State<AddEventScreen> {
                 padding: const EdgeInsets.only(
                     left: 21, top: 0, right: 21, bottom: 21),
                 child: TextFormField(
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please Enter Title';
-                    }
-                    return null;
-                  },
+
+                  controller: _title,
+
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.cyan[50],
@@ -150,12 +243,7 @@ class _AddEventScreenState extends State<AddEventScreen> {
                 padding: const EdgeInsets.only(
                     left: 21, top: 0, right: 21, bottom: 21),
                 child: TextFormField(
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please Enter Description';
-                    }
-                    return null;
-                  },
+                  controller: _des,
                   maxLines: 5,
                   decoration: InputDecoration(
                     filled: true,
@@ -215,12 +303,7 @@ class _AddEventScreenState extends State<AddEventScreen> {
                 padding: const EdgeInsets.only(
                     left: 21, top: 0, right: 21, bottom: 21),
                 child: TextFormField(
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please Enter Venue';
-                    }
-                    return null;
-                  },
+                  controller: _venue,
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.cyan[50],
@@ -279,12 +362,16 @@ class _AddEventScreenState extends State<AddEventScreen> {
                 padding: const EdgeInsets.only(
                     left: 21, top: 0, right: 21, bottom: 21),
                 child: TextFormField(
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please Enter Date';
-                    }
-                    return null;
+                  onTap: () {
+                    _selectDate(context);
+
                   },
+                  controller: _dateController,
+                  onSaved: (String val) {
+                    _setDate = val;
+                  },
+
+
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.cyan[50],
@@ -343,12 +430,15 @@ class _AddEventScreenState extends State<AddEventScreen> {
                 padding: const EdgeInsets.only(
                     left: 21, top: 0, right: 21, bottom: 21),
                 child: TextFormField(
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please Enter Time';
-                    }
-                    return null;
+                  onTap: () {
+                    _selectTime(context);
+                    title: Text(_timeController.toString());
                   },
+                  controller: _timeController,
+                  onSaved: (String val) {
+                    _setTime = val;
+                  },
+
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.cyan[50],
@@ -388,7 +478,7 @@ class _AddEventScreenState extends State<AddEventScreen> {
               ),
               //Add poster
               InkWell(
-                onTap: pickImage,
+                onTap: getImage,
                 child: Padding(
                   padding: const EdgeInsets.only(
                       top: 0, bottom: 11, left: 21, right: 21),
@@ -401,25 +491,25 @@ class _AddEventScreenState extends State<AddEventScreen> {
                       child: Center(
                         child: _image == null
                             ? Text(
-                                'Select Poster',
-                                style: GoogleFonts.openSans(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.blue[900],
-                                  letterSpacing: 0.5,
-                                  fontSize: 21,
-                                ),
-                              )
+                          'Select Poster',
+                          style: GoogleFonts.openSans(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue[900],
+                            letterSpacing: 0.5,
+                            fontSize: 21,
+                          ),
+                        )
                             : Image.file(_image),
                       )),
                 ),
+
               ),
               //Add Button
               RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    return true;
-                  }
-                },
+                onPressed: (){
+                  uploadPic(context);
+                  addevent();
+                      },
                 child: Padding(
                   padding: const EdgeInsets.all(11),
                   child: Text(
@@ -447,4 +537,27 @@ class _AddEventScreenState extends State<AddEventScreen> {
       ),
     );
   }
-}
+
+
+  void addevent()
+  {
+    String title=_title.text;
+    String description=_des.text;
+    String venue=_venue.text;
+    String date=_dateController.text;
+    String time=_timeController.text;
+
+
+    Map<String,String> event={
+      'title':title,
+      'description':description,
+      'venue':venue,
+      'date':date,
+      'time':time
+    };
+
+    _ref.push().set(event).then((value)
+    {});
+
+  }
+  }
