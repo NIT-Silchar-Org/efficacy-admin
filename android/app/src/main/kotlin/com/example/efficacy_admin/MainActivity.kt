013ed5250df2ca2d037e48b86fe1d5e2fb54012b
Biju Borah <biju20_ug@ee.nits.ac.in>
2021-11-22 17:41:42 +0530

    Revert "Merge pull request #2 from NIT-Silchar-Org/sumit"
    This reverts commit d88e006c024193b0708468fffbfa68430247454e, reversing
    changes made to b20c73ec93aa82f9d3a34672ef30e8117286b62b.

--- android/app/src/main/kotlin/com/example/efficacy_admin/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.efficacy_admin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/themes/app_theme.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'theme.dart';
-
-class AppTheme {
-  static ThemeData light = lightTheme;
-  static ThemeData dark = darkTheme;
-}
