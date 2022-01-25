10611a940699ad1c670807975e195b0ca556acbc
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-01-25 16:26:04 +0530

    cleaned up code

---
+++ lib/widgets/form_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:efficacy_admin/themes/appcolor.dart';
+
+class FormWidget extends StatelessWidget {
+  String text;
+  IconData icons;
+  int? line;
+  FormWidget({Key? key, required this.text, required this.icons, this.line})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      maxLines: line ?? 1,
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+          icons,
+          color: AppColorLight.outline,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: AppColorLight.outline, width: 2.0),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide:
+              BorderSide(color: Theme.of(context).primaryColor, width: 2.0),
+        ),
+        labelText: text,
+        labelStyle: TextStyle(color: AppColorLight.outline),
+      ),
+    );
+  }
+}
