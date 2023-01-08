ed40f62e938d2f72943663870acb825566c28166
subhajit735 <subhajit735@gmail.com>
2023-01-08 23:30:55 +0530

    render the event detail

--- lib/utils/share_widget.dart
+++ lib/utils/share_widget.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
 class Share extends StatelessWidget {
-  const Share({Key? key}) : super(key: key);
+  Share({Key? key, this.date_of_post}) : super(key: key);
+  DateTime? date_of_post;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         const Icon(
-          Icons.share_outlined,
+          FontAwesomeIcons.clock,
           size: 20,
           color: Color(0xff05354C),
         ),
         Text(
-          'Share',
+          DateFormat(DateFormat.ABBR_MONTH_DAY)
+              .format(date_of_post!)
+              .toString()+", "+DateFormat(DateFormat.HOUR_MINUTE)
+              .format(date_of_post!)
+              .toString(),
           style: GoogleFonts.poppins(
             textStyle: TextStyle(
               color: const Color(0xff49454F).withOpacity(0.7),
